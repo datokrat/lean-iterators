703727e57d7db659ad46a6973f5355bf71009138
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-04-02 10:00:00 +0200

    productive instances

--- Iterator/Combinators.lean
+++ Iterator/Combinators.lean
@@ -25,6 +25,9 @@ instance {m} [Functor m] [Iterator α m β] : Iterator (Iter (α := α) m β) m 
 instance [Functor m] [Iterator α m β] [Finite α] : Finite (Iter (α := α) m β) where
   wf := InvImage.wf (finiteIteratorWF ∘ Iter.inner ∘ FiniteIteratorWF.inner) Finite.wf
 
+instance [Functor m] [Iterator α m β] [Productive α] : Productive (Iter (α := α) m β) where
+  wf := InvImage.wf (productiveIteratorWF ∘ Iter.inner ∘ ProductiveIteratorWF.inner) Productive.wf
+
 def Iter.step [Functor m] [Iterator α m β] (it : Iter (α := α) m β) :=
   Iterator.step it
 
@@ -48,7 +51,7 @@ instance [Iterator α m β] [Functor m] : Iterator (FilterMap α f) m γ where
     | .skip it' h => .skip ⟨it'⟩ (.inr h)
     | .done h => .done h) <$> Iterator.step it.inner
 
-theorem FilterMap.subrel [Functor m] [Iterator α m β] :
+theorem FilterMap.finiteIteratorWF_subrelation [Functor m] [Iterator α m β] :
     Subrelation
       (FiniteIteratorWF.lt (α := (FilterMap α f)))
       (InvImage FiniteIteratorWF.lt (finiteIteratorWF ∘ FilterMap.inner ∘ FiniteIteratorWF.inner)) := by
@@ -59,8 +62,19 @@ theorem FilterMap.subrel [Functor m] [Iterator α m β] :
   · exact Or.inl ⟨b', h.2⟩
   · exact Or.inr h
 
+theorem FilterMap.productiveIteratorWF_subrelation [Functor m] [Iterator α m β] :
+    Subrelation
+      (ProductiveIteratorWF.lt (α := (FilterMap α (some ∘ f))))
+      (InvImage ProductiveIteratorWF.lt (productiveIteratorWF ∘ FilterMap.inner ∘ ProductiveIteratorWF.inner)) := by
+  intro x y hlt
+  simp only [ProductiveIteratorWF.lt, Iterator.yielded, Iterator.skipped] at hlt
+  obtain ⟨_, h, _⟩ | h := hlt
+  · contradiction
+  · exact h
+
 instance [Functor m] [Iterator α m β] [Finite α] : Finite (FilterMap α f) where
-  wf := FilterMap.subrel.wf <| InvImage.wf (finiteIteratorWF ∘ FilterMap.inner ∘ FiniteIteratorWF.inner) Finite.wf
+  wf := FilterMap.finiteIteratorWF_subrelation.wf <|
+    InvImage.wf (finiteIteratorWF ∘ FilterMap.inner ∘ FiniteIteratorWF.inner) Finite.wf
 
 @[inline]
 def Iter.filterMap [Iterator α m β] [Functor m] (f : β → Option γ) (it : Iter (α := α) m β) :
