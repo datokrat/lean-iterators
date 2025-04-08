125a69876fc3287e217a4d9b4842ce36077332c9
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-04-08 10:18:08 +0200

    mwe snapshot

--- Iterator/MWE.lean
+++ Iterator/MWE.lean
@@ -1,123 +1,77 @@
-
 variable {α : Type u} {β : Type v}
 
-inductive IterStep (α β) (yielded : α → β → Prop) (skipped : α → Prop) (finished : Prop) where
-| yield : (it : α) → (b : β) → yielded it b → IterStep α β yielded skipped finished
-| skip : (a : α) → skipped a → IterStep α β yielded skipped finished
-| done : finished → IterStep α β yielded skipped finished
+inductive Step (α β) where
+| yield : (it : α) → (b : β) → Step α β
+| skip : (a : α) → Step α β
+| done : Step α β
 
-class Iterator (α : Type u) (m : outParam (Type (max u v) → Type x)) (β : outParam (Type v)) where
-  yielded : α → α → β → Prop
-  skipped : α → α → Prop
-  finished : α → Prop
-  step : (a : α) → m (IterStep α β (yielded a) (skipped a) (finished a))
+class Iterator (α : Type u) (β : outParam (Type v)) where
+  step : (it : α) → { s : Step α β // match s with
+      | .yield it' b => True
+      | .skip it' => True
+      | .done => True }
 
-abbrev IterStep.for {α β m} [Iterator α m β] (it : α) := IterStep α β (Iterator.yielded it) (Iterator.skipped it) (Iterator.finished it)
+abbrev Step.for {α β} [Iterator α β] (it : α) := { s : Step α β // match s with
+    | .yield it' b => True
+    | .skip it' => True
+    | .done => True }
 
-inductive RawStep (α β) where
-| yield : (it : α) → (b : β) → RawStep α β
-| skip : (a : α) → RawStep α β
-| done : RawStep α β
-
-section Wrapper
-
-structure Iter (m) (β : Type v) [Iterator α m β] where
+structure Wrapper (α β) [Iterator α β] where
   inner : α
 
 @[inline]
-def toIter {m} [Iterator α m β] (it : α) : Iter (α := α) m β :=
+def toWrapper [Iterator α β] (it : α) : Wrapper α β :=
   ⟨it⟩
 
-instance {m} [Functor m] [Iterator α m β] : Iterator (Iter (α := α) m β) m β where
-  yielded it it' b := Iterator.yielded it.inner it'.inner b
-  skipped it it' := Iterator.skipped it.inner it'.inner
-  finished it := Iterator.finished it.inner
-  step it := (match · with
-    | .yield it' x h => .yield ⟨it'⟩ x h
-    | .skip it' h => .skip ⟨it'⟩ h
-    | .done h => .done h) <$> (Iterator.step it.inner)
+instance [Iterator α β] : Iterator (Wrapper α β) β where
+  step it := match Iterator.step it.inner with
+    | ⟨.yield it' x, h⟩ => ⟨.yield ⟨it'⟩ x, h⟩
+    | ⟨.skip it', h⟩ => ⟨.skip ⟨it'⟩, h⟩
+    | ⟨.done, h⟩ => ⟨.done, h⟩
 
-end Wrapper
-
-section ListIterator
-
-variable {m : Type u → Type u}
-
-structure ListIterator (α : Type u) (m : Type u → Type u) where
+structure ListIterator (α : Type u) where
   list : List α
 
-instance [Pure m] : Iterator (ListIterator α m) m α where
-  yielded it it' a := sorry
-  skipped it it' := sorry
-  finished it := sorry
+instance : Iterator (ListIterator α) α where
   step
-    | { list := .nil } => pure <| .done sorry
-    | { list := x :: xs } => pure <| .yield { list := xs } x sorry
+    | { list := .nil } => ⟨.done, sorry⟩
+    | { list := x :: xs } => ⟨.yield { list := xs } x, sorry⟩
 
 @[inline]
-def List.iter {α} (l : List α) (m := Id) [Pure m] : Iter (α := ListIterator α m) m α :=
-  toIter { list := l }
+def List.iter {α} (l : List α) : Wrapper (ListIterator α) α :=
+  toWrapper { list := l }
 
-end ListIterator
-
-section AbstractIteration
-
-structure Iteration (m : Type u → Type v) (γ : Type u) where
+structure Iteration (γ : Type u) where
   prop : γ → Prop
-  elem : m { c // prop c }
-
-instance (m) [Monad m] : Monad (Iteration m) where
-  pure c := { prop c' := sorry, elem := Pure.pure ⟨c, sorry⟩ }
-  bind t f := {
-    prop d := d = d -- some nontrivial term (not sorry!) so that optimizations don't trigger
-    elem := do (fun x => ⟨x.1, sorry⟩) <$> (f (← t.elem).val).elem }
+  elem : { c // prop c }
 
 @[inline]
-def Iteration.step {α : Type u} {β : Type v} [Iterator α m β] [Functor m] (it : α) : Iteration m (IterStep.for it) :=
-  { prop := sorry,
-    elem := (fun step => ⟨step, sorry⟩) <$> Iterator.step it }
+def Iteration.instIterator (stepFn : α → Iteration (Step α β)) : Iterator α β where
+  step it := (match (stepFn it).elem with
+    | ⟨.yield it' b, h⟩ => ⟨.yield it' b, sorry⟩
+    | ⟨.skip it', h⟩ => ⟨.skip it', sorry⟩
+    | ⟨.done, h⟩ => ⟨.done, sorry⟩)
 
-@[inline]
-def Iteration.instIterator [Functor m] (stepFn : α → Iteration m (RawStep α β)) : Iterator α m β where
-  yielded it it' b := sorry --(stepFn it).prop (.yield it' b ⟨⟩)
-  skipped it it' := (stepFn it).prop (.skip it')
-  finished it := (stepFn it).prop (.done)
-  step it := (match · with
-    | ⟨.yield it' b, h⟩ => .yield it' b sorry
-    | ⟨.skip it', h⟩ => .skip it' h
-    | ⟨.done, h⟩ => .done h) <$> (stepFn it).elem
+structure MyIterator (α β α' : Type) [Iterator α β] (f : β → α') where
+  inner : Option α'
 
-end AbstractIteration
-
-section Combinators
-
-structure FlatMap2 (α β α' : Type) [Iterator α Id β] (f : β → α') where
-  it₂ : Option α'
-
-instance {α β α' β' : Type} [Iterator α Id β] [Iterator α' Id β'] (f : β → α') : Iterator (FlatMap2 α β α' f) Id β' :=
+instance {α β α' β' : Type} [Iterator α β] [Iterator α' β'] (f : β → α') : Iterator (MyIterator α β α' f) β' :=
   Iteration.instIterator fun
-    | { it₂ := none } => pure <| .done
-    | { it₂ := some it₂ } => do
-      ⟨fun _ => True, match ← { prop := sorry, elem := ⟨Iterator.step it₂, sorry⟩ } with
-      | .yield it' b _ => ⟨.yield { it₂ := some it' } b, sorry⟩
-      | .skip it' _ => ⟨.skip { it₂ := some it' }, sorry⟩
-      | .done _ => ⟨.done, sorry⟩⟩
-
-end Combinators
-
-section IR
+    | ⟨none⟩ => { prop := sorry, elem := ⟨.done, sorry⟩ }
+    | ⟨some it⟩ => { prop d := d = d
+                     elem := ⟨match Iterator.step it with
+                              | ⟨.yield it' b, _⟩ => .yield ⟨it'⟩ b
+                              | _ => .done, sorry⟩ }
 
 set_option trace.compiler.ir.result true in
-def testFlatMap (l : List (List Nat)) : Nat :=
-  go (FlatMap2.mk (α := ListIterator (List Nat) Id) (f := List.iter) none) 0
+def testMyIterator (l : List (List Nat)) : Nat :=
+  go (MyIterator.mk (α := ListIterator (List Nat)) (f := List.iter) (inner := none)) 0
 where
   @[specialize]
   go it acc :=
     match Iterator.step it with
-    | .yield it' n _ => go it' (acc + n)
-    | .skip it' _ => go it' acc
-    | .done _ => acc
+    | ⟨.yield it' n, _⟩ => go it' (acc + n)
+    | ⟨.skip it', _⟩ => go it' acc
+    | ⟨.done, _⟩ => acc
   termination_by it
   decreasing_by all_goals sorry
-
-end IR
