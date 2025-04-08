ff0783470ef65e364156d36e989a0d884a72a372
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-04-08 09:28:40 +0200

    mwe snapshot

--- Iterator/MWE.lean
+++ Iterator/MWE.lean
@@ -6,17 +6,18 @@ inductive IterStep (α β) (yielded : α → β → Prop) (skipped : α → Prop
 | skip : (a : α) → skipped a → IterStep α β yielded skipped finished
 | done : finished → IterStep α β yielded skipped finished
 
-set_option pp.all true in
-def IterStep.successor {yp sp fp} : IterStep α β yp sp fp → Option α
-  | .yield it _ _ => some it
-  | .skip it _ => some it
-  | .done _ => none
-
 class Iterator (α : Type u) (m : outParam (Type (max u v) → Type x)) (β : outParam (Type v)) where
   yielded : α → α → β → Prop
   skipped : α → α → Prop
   finished : α → Prop
   step : (a : α) → m (IterStep α β (yielded a) (skipped a) (finished a))
+
+abbrev IterStep.for {α β m} [Iterator α m β] (it : α) := IterStep α β (Iterator.yielded it) (Iterator.skipped it) (Iterator.finished it)
+
+inductive RawStep (α β) where
+| yield : (it : α) → (b : β) → RawStep α β
+| skip : (a : α) → RawStep α β
+| done : RawStep α β
 
 section Wrapper
 
@@ -61,26 +62,15 @@ end ListIterator
 
 section AbstractIteration
 
-abbrev RawStep (α β) := IterStep α β (fun _ _ => True) (fun _ => True) True
-
-abbrev IterStep.for {α β m} [Iterator α m β] (it : α) := IterStep α β (Iterator.yielded it) (Iterator.skipped it) (Iterator.finished it)
-
 structure Iteration (m : Type u → Type v) (γ : Type u) where
   prop : γ → Prop
   elem : m { c // prop c }
 
-@[inline]
-def Iteration.pure {γ m} [Pure m] (c : γ) : Iteration m γ :=
-  { prop c' := sorry, elem := Pure.pure ⟨c, sorry⟩ }
-
-@[inline]
-def Iteration.bind {γ δ m} [Monad m] (t : Iteration m γ) (f : γ → Iteration m δ) : Iteration m δ :=
-  { prop d := d = d -- some nontrivial term (not sorry!) so that optimizations don't trigger
-    elem := do (fun x => ⟨x.1, sorry⟩) <$> (f (← t.elem).val).elem }
-
 instance (m) [Monad m] : Monad (Iteration m) where
-  pure := Iteration.pure
-  bind := Iteration.bind
+  pure c := { prop c' := sorry, elem := Pure.pure ⟨c, sorry⟩ }
+  bind t f := {
+    prop d := d = d -- some nontrivial term (not sorry!) so that optimizations don't trigger
+    elem := do (fun x => ⟨x.1, sorry⟩) <$> (f (← t.elem).val).elem }
 
 @[inline]
 def Iteration.step {α : Type u} {β : Type v} [Iterator α m β] [Functor m] (it : α) : Iteration m (IterStep.for it) :=
@@ -90,12 +80,12 @@ def Iteration.step {α : Type u} {β : Type v} [Iterator α m β] [Functor m] (i
 @[inline]
 def Iteration.instIterator [Functor m] (stepFn : α → Iteration m (RawStep α β)) : Iterator α m β where
   yielded it it' b := sorry --(stepFn it).prop (.yield it' b ⟨⟩)
-  skipped it it' := (stepFn it).prop (.skip it' ⟨⟩)
-  finished it := (stepFn it).prop (.done ⟨⟩)
+  skipped it it' := (stepFn it).prop (.skip it')
+  finished it := (stepFn it).prop (.done)
   step it := (match · with
-    | ⟨.yield it' b _, h⟩ => .yield it' b sorry
-    | ⟨.skip it' _, h⟩ => .skip it' h
-    | ⟨.done _, h⟩ => .done h) <$> (stepFn it).elem
+    | ⟨.yield it' b, h⟩ => .yield it' b sorry
+    | ⟨.skip it', h⟩ => .skip it' h
+    | ⟨.done, h⟩ => .done h) <$> (stepFn it).elem
 
 end AbstractIteration
 
@@ -106,12 +96,12 @@ structure FlatMap2 (α β α' : Type) [Iterator α Id β] (f : β → α') where
 
 instance {α β α' β' : Type} [Iterator α Id β] [Iterator α' Id β'] (f : β → α') : Iterator (FlatMap2 α β α' f) Id β' :=
   Iteration.instIterator fun
-    | { it₂ := none } => pure <| .done ⟨⟩
+    | { it₂ := none } => pure <| .done
     | { it₂ := some it₂ } => do
       ⟨fun _ => True, match ← { prop := sorry, elem := ⟨Iterator.step it₂, sorry⟩ } with
-      | .yield it' b _ => ⟨.yield { it₂ := some it' } b ⟨⟩, sorry⟩
-      | .skip it' _ => ⟨.skip { it₂ := some it' } ⟨⟩, sorry⟩
-      | .done _ => ⟨.done ⟨⟩, sorry⟩⟩
+      | .yield it' b _ => ⟨.yield { it₂ := some it' } b, sorry⟩
+      | .skip it' _ => ⟨.skip { it₂ := some it' }, sorry⟩
+      | .done _ => ⟨.done, sorry⟩⟩
 
 end Combinators
 
@@ -128,8 +118,6 @@ where
     | .skip it' _ => go it' acc
     | .done _ => acc
   termination_by it
-  decreasing_by
-    · sorry
-    · sorry
+  decreasing_by all_goals sorry
 
 end IR
