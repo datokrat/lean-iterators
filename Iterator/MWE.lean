61a586ab0dc7c4dffe4579f00be5b407e80c7ec3
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-04-08 10:56:31 +0200

    mwe snapshot

--- Iterator/MWE.lean
+++ Iterator/MWE.lean
@@ -1,33 +1,10 @@
-variable {α : Type u} {β : Type v}
-
 inductive Step (α β) where
 | yield : (it : α) → (b : β) → Step α β
 | skip : (a : α) → Step α β
 | done : Step α β
 
 class Iterator (α : Type u) (β : outParam (Type v)) where
-  step : (it : α) → { s : Step α β // match s with
-      | .yield it' b => True
-      | .skip it' => True
-      | .done => True }
-
-abbrev Step.for {α β} [Iterator α β] (it : α) := { s : Step α β // match s with
-    | .yield it' b => True
-    | .skip it' => True
-    | .done => True }
-
-structure Wrapper (α β) [Iterator α β] where
-  inner : α
-
-@[inline]
-def toWrapper [Iterator α β] (it : α) : Wrapper α β :=
-  ⟨it⟩
-
-instance [Iterator α β] : Iterator (Wrapper α β) β where
-  step it := match Iterator.step it.inner with
-    | ⟨.yield it' x, h⟩ => ⟨.yield ⟨it'⟩ x, h⟩
-    | ⟨.skip it', h⟩ => ⟨.skip ⟨it'⟩, h⟩
-    | ⟨.done, h⟩ => ⟨.done, h⟩
+  step : (it : α) → { s : Step α β // True }
 
 structure ListIterator (α : Type u) where
   list : List α
@@ -38,30 +15,25 @@ instance : Iterator (ListIterator α) α where
     | { list := x :: xs } => ⟨.yield { list := xs } x, sorry⟩
 
 @[inline]
-def List.iter {α} (l : List α) : Wrapper (ListIterator α) α :=
-  toWrapper { list := l }
-
-structure Iteration (γ : Type u) where
-  prop : γ → Prop
-  elem : { c // prop c }
+def List.iter {α} (l : List α) : ListIterator α :=
+  ⟨l⟩
 
 @[inline]
-def Iteration.instIterator (stepFn : α → Iteration (Step α β)) : Iterator α β where
-  step it := (match (stepFn it).elem with
-    | ⟨.yield it' b, h⟩ => ⟨.yield it' b, sorry⟩
-    | ⟨.skip it', h⟩ => ⟨.skip it', sorry⟩
-    | ⟨.done, h⟩ => ⟨.done, sorry⟩)
+def Iteration.instIterator (stepFn : α → { s : (Step α β) // True }) : Iterator α β where
+  step it := (match stepFn it with
+    | ⟨.yield it' b, _⟩ => ⟨.yield it' b, sorry⟩
+    | ⟨.skip it', _⟩ => ⟨.skip it', sorry⟩
+    | ⟨.done, _⟩ => ⟨.done, sorry⟩)
 
 structure MyIterator (α β α' : Type) [Iterator α β] (f : β → α') where
   inner : Option α'
 
 instance {α β α' β' : Type} [Iterator α β] [Iterator α' β'] (f : β → α') : Iterator (MyIterator α β α' f) β' :=
   Iteration.instIterator fun
-    | ⟨none⟩ => { prop := sorry, elem := ⟨.done, sorry⟩ }
-    | ⟨some it⟩ => { prop d := d = d
-                     elem := ⟨match Iterator.step it with
+    | ⟨none⟩ => ⟨.done, sorry⟩
+    | ⟨some it⟩ => ⟨match Iterator.step it with
                               | ⟨.yield it' b, _⟩ => .yield ⟨it'⟩ b
-                              | _ => .done, sorry⟩ }
+                              | _ => .done, sorry⟩
 
 set_option trace.compiler.ir.result true in
 def testMyIterator (l : List (List Nat)) : Nat :=
